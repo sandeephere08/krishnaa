2efbfa42216b045ca0f1dc662e2c8c80460583a2
hide-sudo <deathhide08@gmail.com>
2025-05-22 22:38:44 +0530

    Add files via upload

---
+++ VIP_INNOCENT/utils/pastebin.py
@@ -0,0 +1,47 @@
+import aiohttp
+
+
+import socket
+from asyncio import get_running_loop
+from functools import partial
+
+
+def _netcat(host, port, content):
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.connect((host, port))
+    s.sendall(content.encode())
+    s.shutdown(socket.SHUT_WR)
+    while True:
+        data = s.recv(4096).decode("utf-8").strip("\n\x00")
+        if not data:
+            break
+        return data
+    s.close()
+
+
+async def paste(content):
+    loop = get_running_loop()
+    link = await loop.run_in_executor(None, partial(_netcat, "ezup.dev", 9999, content))
+    return link
+
+####2nd paste code 
+
+BASE = "https://batbin.me/"
+
+
+async def post(url: str, *args, **kwargs):
+    async with aiohttp.ClientSession() as session:
+        async with session.post(url, *args, **kwargs) as resp:
+            try:
+                data = await resp.json()
+            except Exception:
+                data = await resp.text()
+        return data
+
+
+async def INNOCENTBin(content):
+    url ="https://pastebin.com/api/api_post.php"
+    data = {"api_dev_key":"9Rfu50iV5l3EuRWATw7EDLuC37RED-C4","api_paste_code": content,"api_option": "paste"}
+    response = requests.post(url, data=data)
+    link=response.text
+    return link
